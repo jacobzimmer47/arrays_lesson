12a3ee80d336c072ffdf75f635663043608149f1
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-06-08 11:19:35 -0600

    Added comments

--- arrays.js
+++ arrays.js
@@ -12,4 +12,10 @@ nums.push();
 
 boolean.shift();
 //the output of "boolean.shift()" will remove the first element from the array,
-//then returns the removed element.
+//then returns the removed element
+
+// Index positions are the number corilated to an element in an array.
+// these Index positions start and 0 and can go on for how many ever elements are in an array
+// For example, "Hello" in var words is 0
+// and, "51" in var nums is 1
+//lastly, "false" is in both 2 and 3

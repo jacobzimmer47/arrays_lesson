79dc7b5f33fa2cc8312487696644f14aa4ef419c
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-06-08 11:13:40 -0600

    added method

--- arrays.js
+++ arrays.js
@@ -3,3 +3,13 @@ var words = ["Hello","Bye", "Seeya", "Today" ];
 var nums = ["10", "51", "87", "14"];
 
 var boolean = ["true", "true", "false", "false"];
+
+words.pop();
+//the output of "words.pop()" will remove the last element from my array
+
+nums.push();
+//the output of "nums.push()" will add to the end of my array.
+
+boolean.shift();
+//the output of "boolean.shift()" will remove the first element from the array,
+//then returns the removed element.
